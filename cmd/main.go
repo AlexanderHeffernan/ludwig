6a2a01ec6b285bd500e9acdc1065bf4b83a4ce4e
Alexander Heffernan <heffernan.alexander@icloud.com>
2026-01-03 22:09:28 +1300

    Add version flag to main and update ldflags in GoReleaser configuration

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"ludwig/internal/cli"
 )
 
+var version = "dev"
+
 func main() {
+	versionFlag := flag.Bool("version", false, "Print the version and exit")
+	flag.Parse()
+
+	if *versionFlag {
+		fmt.Println("ludwig version " + version)
+		return
+	}
+
 	cli.StartInteractive()
 }

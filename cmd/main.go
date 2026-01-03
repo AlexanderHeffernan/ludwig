3b116f99b67078596c0f0d9d68a444271a8dab86
Alexander Heffernan <heffernan.alexander@icloud.com>
2026-01-03 22:16:50 +1300

    Add update functionality to check for and install the latest version

--- cmd/main.go
+++ cmd/main.go
@@ -4,16 +4,26 @@ import (
 	"flag"
 	"fmt"
 	"ludwig/internal/cli"
+	"ludwig/internal/updater"
 )
 
 var version = "dev"
 
 func main() {
 	versionFlag := flag.Bool("version", false, "Print the version and exit")
+	updateFlag := flag.Bool("update", false, "Check for and install updates")
 	flag.Parse()
 
 	if *versionFlag {
 		fmt.Println("ludwig version " + version)
+		return
+	}
+
+	if *updateFlag {
+		if err := updater.DownloadAndInstall(); err != nil {
+			fmt.Println("Error: " + err.Error())
+			return
+		}
 		return
 	}
 

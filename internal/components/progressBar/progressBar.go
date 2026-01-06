d23230e56e5e0b5f0b4a7cebdc8e4984a574bc72
OliverHeffernan <oliverheffernan@icloud.com>
2026-01-06 20:03:20 +1300

    removed another log

--- internal/components/progressBar/progressBar.go
+++ internal/components/progressBar/progressBar.go
@@ -7,7 +7,6 @@ import (
 	"github.com/charmbracelet/lipgloss"
 	tea "github.com/charmbracelet/bubbletea"
 	"ludwig/internal/utils"
-	"strconv"
 )
 
 type Model struct {
@@ -29,22 +28,17 @@ func (m *Model) View() string {
 	if m.Width == 0 {
 		m.Width = utils.TermWidth()
 	}
-	utils.DebugLog("ProgressBar")
 	floatWidth := float64(m.Width)
 	//m.Progress = m.Viewport.ScrollPercent()
 	barWidth := floatWidth * m.Progress
 
 	intWidth := int(math.Round(barWidth))
-	utils.DebugLog("ProgressBar Width: " + strconv.Itoa(m.Width) + " Progress: " + strconv.FormatFloat(m.Progress, 'f', 2, 64) + " intWidth: " + strconv.Itoa(intWidth))
 	intEmptyWidth := m.Width - intWidth
 	bar := barStyle.Render(strings.Repeat("─", intWidth)) + style.Render(strings.Repeat("─", intEmptyWidth))
 	return bar
 }
 
 func (m *Model) Update(msg tea.Msg) (*Model, tea.Cmd) {
-	utils.DebugLog("ProgressBar Update")
-	//m.Progress = m.Viewport.ScrollPercent()
-	utils.DebugLog("ProgressBar ScrollPercent: " + strconv.FormatFloat(m.Progress, 'f', 2, 64))
 
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
